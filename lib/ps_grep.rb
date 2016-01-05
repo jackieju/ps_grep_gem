9ae400683135bdc9ee7fc9adcc30c034c2414876
jackie <jackie.ju@gmail.com>
2016-01-06 03:29:39 +0800

    first commit code

---
+++ lib/ps_grep.rb
@@ -0,0 +1,69 @@
+# ps_grep
+# Find a process using name pattern
+# Author: Jackie.ju@gmail.com
+# 
+require 'rubygems'
+require 'rbconfig'
+
+s = Config::CONFIG['target_os']
+if s.index("linux")
+    $_OS_TYPE = "linux"
+elsif s.index("darwin")
+    $_OS_TYPE = "darwin"
+end
+
+def find_process(*pattern)
+    mongrels = []
+    
+    s = pattern.join(" | grep ")
+
+    command = "ps aux | grep -v grep | grep #{s}"
+    p "command=>#{command}"
+    r = `#{command}`
+    print "==>command output<===\n#{r}"
+    print "==>command output end<===\n"
+    
+    if $_OS_TYPE == "linux"
+    
+    
+        r.scan(/^(.*?)\s+(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+(.*?)\s+(.*?)\s+(\d+:\d+\.\d+)\s+(.*?)$/){|m|
+            mongrels.push({
+                :user=>m[0].strip,
+                :pid=>m[1],
+                :cpu=>m[2],
+                :mem1=>m[3],
+                :mem2=>m[4],
+                :mem3=>m[5],
+                :start=>m[8],
+                :time=>m[9],
+                :cmd=>m[10].strip
+            })
+        }
+    elsif $_OS_TYPE == "darwin"
+        r.scan(/^(.*?)\s+(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+)\s+(\d+)\s+(.*?)\s+(.*?)\s+(.*?)\s+(\d+:\d+\.\d+)\s+(.*?)$/){|m|
+            mongrels.push({
+                :user=>m[0].strip,
+                :pid=>m[1],
+                :cpu=>m[2],
+                :mem1=>m[3],
+                :mem2=>m[4],
+                :mem3=>m[5],
+                :start=>m[8],
+                :time=>m[9],
+                :cmd=>m[10].strip
+            })
+        }
+    else
+        p "OS #{$_OS_TYPE} is not supported"
+    end 
+    
+    return mongrels
+end
+
+
+# test
+=begin
+ret =  find_process("ruby", "")
+p "====>"
+p ret
+=end
---
+++ ps_grep.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = 'ps_grep'
+  s.version     = '0.0.1'
+  s.date        = '2016-01-05'
+  s.summary     = "Find a process info by pattern!"
+  s.description = "Find a process info by pattern!"
+  s.authors     = ["Ju Weihua (Jackie Ju)"]
+  s.email       = 'jackie.ju@gmail.com'
+  s.files       = ["lib/ps_grep.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/ps_grep'
+  s.license       = 'MIT'
+  # add executables
+  s.executables << 'ps_grep'
+end
